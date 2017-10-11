0ebed286a15f79a323a832b95a2a5601ddcb2df2
Ian V Naeger <ianvnaeger@mwc-016178.dhcp.missouri.edu>
2017-10-11 09:36:50 -0500

    Added Arduino Portion

--- BartenderBluetoothConnector.cpp
+++ BartenderBluetoothConnector.cpp
@@ -14,7 +14,7 @@ int main( int argc, char **argv ){
              A: SampleDrink1\n B: SampleDrink2\n C: SampleDrink3\n D: SampleDrink4\n E: SampleDrink5\n");
         return 0;
     }
-    if( argv[1] != "A" || argv[1] != "B" || argv[1] != "C" || argv[1] != "D" || argv[1] != "E" ){
+    if( argv[1] != "A" || argv[1] != "B" || argv[1] != "C" || argv[1] != "D" ){
         printf("You have selected an invalid drink option. \nPlease select one of the following:\n
             A: SampleDrink1\n B: SampleDrink2\n C: SampleDrink3\n D: SampleDrink4\n E: SampleDrink5\n");
         return 0;
